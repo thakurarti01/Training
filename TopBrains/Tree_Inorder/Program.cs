bfd6e896606452dd2e8384e05e3c3c6d5101099b
Arti Thakur <arti31thakur@gmail.com>
2026-08-01 10:01:14 +0530

    Code

---
+++ TopBrains/Tree_Inorder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int value)
+    {
+        data = value;
+        left = null;
+        right = null;
+    }
+}
+
+class Program
+{
+    // Inorder Traversal
+    static void InOrder(Node root)
+    {
+        if (root == null)
+            return;
+
+        InOrder(root.left);          // Visit Left
+        Console.Write(root.data + " "); // Visit Root
+        InOrder(root.right);         // Visit Right
+    }
+
+    static void Main(string[] args)
+    {
+        // Creating the tree
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        Console.WriteLine("Inorder Traversal:");
+        InOrder(root);
+    }
+}
---
+++ TopBrains/Tree_Preorder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int value)
+    {
+        data = value;
+        left = null;
+        right = null;
+    }
+}
+
+class Program
+{
+    // Preorder Traversal
+    static void PreOrder(Node root)
+    {
+        if (root == null)
+            return;
+
+        Console.Write(root.data + " "); // Visit Root
+        PreOrder(root.left);            // Visit Left
+        PreOrder(root.right);           // Visit Right
+    }
+
+    static void Main(string[] args)
+    {
+        // Creating the tree
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        Console.WriteLine("Preorder Traversal:");
+        PreOrder(root);
+    }
+}
