9ec08ba60ebe22d6b1525908a0d7153d045d8c77
Arti Thakur <arti31thakur@gmail.com>
2026-08-01 10:08:50 +0530

    Code

---
+++ TopBrains/Tree_Postorder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int value)
+    {
+        data = value;
+        left = null;
+        right = null;
+    }
+}
+
+class Program
+{
+    // Postorder Traversal
+    static void PostOrder(Node root)
+    {
+        if (root == null)
+            return;
+
+        PostOrder(root.left);          // Visit Left
+        PostOrder(root.right);         // Visit Right
+        Console.Write(root.data + " "); // Visit Root
+    }
+
+    static void Main(string[] args)
+    {
+        // Creating the tree
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        Console.WriteLine("Postorder Traversal:");
+        PostOrder(root);
+    }
+}
