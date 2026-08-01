1bdb17f167f128151d7c627a0a136b87de6254e9
Arti Thakur <arti31thakur@gmail.com>
2026-08-01 10:05:49 +0530

    Code

---
+++ TopBrains/Node_at_Head/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node next;
+
+    public Node(int value)
+    {
+        data = value;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    public Node head;
+
+    // Insert at Head
+    public void InsertAtHead(int value)
+    {
+        Node newNode = new Node(value);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Display Linked List
+    public void Display()
+    {
+        Node temp = head;
+
+        while (temp != null)
+        {
+            Console.Write(temp.data + " ");
+            temp = temp.next;
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        list.InsertAtHead(10);
+        list.InsertAtHead(20);
+        list.InsertAtHead(30);
+        list.InsertAtHead(40);
+
+        Console.WriteLine("Linked List:");
+        list.Display();
+    }
+}
---
+++ TopBrains/Node_at_Tail/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node next;
+
+    public Node(int value)
+    {
+        data = value;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    public Node head;
+
+    // Insert at Tail
+    public void InsertAtTail(int value)
+    {
+        Node newNode = new Node(value);
+
+        // If the list is empty
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+
+        // Traverse to the last node
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+
+        // Link the last node to the new node
+        temp.next = newNode;
+    }
+
+    // Display the linked list
+    public void Display()
+    {
+        Node temp = head;
+
+        while (temp != null)
+        {
+            Console.Write(temp.data + " ");
+            temp = temp.next;
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        list.InsertAtTail(10);
+        list.InsertAtTail(20);
+        list.InsertAtTail(30);
+        list.InsertAtTail(40);
+
+        Console.WriteLine("Linked List:");
+        list.Display();
+    }
+}
