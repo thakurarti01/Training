d321114250257cd6f937ea0daddd58bae57a311c
Arti Thakur <arti31thakur@gmail.com>
2026-08-04 15:00:57 +0530

    Code

---
+++ DAY_07/Binary_Search/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class BinarySearch
+{
+    static int Search(int[] arr, int key)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        // Continue until the search space becomes empty
+        while (left <= right)
+        {
+            // Find the middle index
+            int mid = (left + right) / 2;
+
+            // If middle element is the key
+            if (arr[mid] == key)
+                return mid;
+
+            // If key is greater, search the right half
+            else if (arr[mid] < key)
+                left = mid + 1;
+
+            // If key is smaller, search the left half
+            else
+                right = mid - 1;
+        }
+
+        // Key not found
+        return -1;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter the number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter the array elements in sorted order:");
+
+        // Take sorted array input from the user
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element " + (i + 1) + ": ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Enter the element to search: ");
+        int key = Convert.ToInt32(Console.ReadLine());
+
+        int result = Search(arr, key);
+
+        if (result != -1)
+            Console.WriteLine("Element found at index: " + result);
+        else
+            Console.WriteLine("Element not found.");
+    }
+}
---
+++ DAY_07/Linear_Search/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class LinearSearch
+{
+    static int Search(int[] arr, int key)
+    {
+        // Check each element one by one
+        for (int i = 0; i < arr.Length; i++)
+        {
+            // If the key is found, return its index
+            if (arr[i] == key)
+                return i;
+        }
+
+        // Key not found
+        return -1;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter the number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter the array elements:");
+
+        // Take array input from the user
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element " + (i + 1) + ": ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Enter the element to search: ");
+        int key = Convert.ToInt32(Console.ReadLine());
+
+        int result = Search(arr, key);
+
+        if (result != -1)
+            Console.WriteLine("Element found at index: " + result);
+        else
+            Console.WriteLine("Element not found.");
+    }
+}
